994c8bf7e35cf6664eb67c5be3fc28af7445cbd8
DlinniyDnepr <dlinniydnepr@gmail.com>
2020-04-13 22:03:20 +0300

    some changes

--- Lesson1.py
+++ Lesson1.py
@@ -1,10 +1,7 @@
 #Printing O and H
 
-print('#'*9,)
-print('#\t\t#')
-print('#\t\t#')
-print('#\t\t#')
-print('#'*9)
+print('#'*9,  ('\n' '#\t\t#')*3 + '\n' + '#'*9)
+
 print()
 print('#\t\t#')
 print('#\t\t#')
