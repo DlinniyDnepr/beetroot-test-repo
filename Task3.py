2dc4467df14f413ee3d1a029f140b1e77f3ff473
DlinniyDnepr <dlinniydnepr@gmail.com>
2020-04-13 18:00:24 +0300

    Create Task3.py

---
+++ Task3.py
@@ -0,0 +1,13 @@
+#Printing O and H
+
+print('#'*9,)
+print('#\t\t#')
+print('#\t\t#')
+print('#\t\t#')
+print('#'*9)
+print()
+print('#\t\t#')
+print('#\t\t#')
+print('#'*9)
+print('#\t\t#')
+print('#\t\t#')
