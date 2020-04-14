8d551759225c8abb735161a79b4db9f428829f94
DlinniyDnepr <63417182+DlinniyDnepr@users.noreply.github.com>
2020-04-14 18:05:41 +0300

    added homework for lesson 2

---
+++ Lesson2.py
@@ -0,0 +1,41 @@
+
+#Task1
+print('Task1')
+#The greeting program.
+
+
+name = 'Artem'
+day = 'April 14'
+print("Good day,", name + '!', day," is a perfect day to learn some python.")
+
+#Task1 using .format
+
+print('Good day,', '{}'.format(name + '!'), '{}'.format(day)," is a perfect day to learn some python.")
+
+#Task2
+print('Task2')
+#Manipulate strings.
+
+name = 'Artem'
+last_name = 'Rada'
+print('Hello my dear', name + ' ' + last_name)
+
+print('Task3')
+#Using python as a calculator.
+
+a = 2
+b = 3
+#Addition
+print('a+b=',a+b)
+#Subtraction
+print('a-b=',a-b)
+#Division
+print('a/b=',a/b)
+#Multiplication
+print('a*b=',a*b)
+#Exponent (Power)
+print('a**b=',a**b)
+#Modulus
+print('a%b=',a%b)
+#Floor division
+print('a//b=',a//b)
