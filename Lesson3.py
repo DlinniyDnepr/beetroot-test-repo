ad8196a8ad93880169fd80634351b2646ab69832
DlinniyDnepr <63417182+DlinniyDnepr@users.noreply.github.com>
2020-04-18 17:05:58 +0300

    Update Lesson3.py
    таск1 добавил strip и else

--- Lesson3.py
+++ Lesson3.py
@@ -1,31 +1,37 @@
 #Task1
 
-x = input('Type anything: ')
-if len(x) > 1:
-    print(x[0:2] + x[-2] + x[-1])
-if len(x) < 2:
-    print('empty string')
+while True:
+    inp = input('Type anything: ')
+    x = inp.strip()    #добавил функцию strip которая убирает все пробелы
+    if len(x) >= 2:
+        print(x[0:2] + x[-2:])
+    else:                       #использую else
+        len(x) < 2
+        print('Empty String')
 
 #Task 2
 #The valid phone number program.
 
-x = input('Please, input your phone number: ')
-while x.isdigit()== False:
-    print('Please, use digits only!')
-    x = input('Please, input your phone number: ')
-while len(x)!= 10:
-    print('Your number should contain 10 digits only!')
-    x = input('Please, input your phone number: ')
-else:
-    print('Thank you!')
+while True:
+        x = input('Please, input your phone number: ')
+        if x ==('stop'):
+            break
+        elif x.isdigit() == True and len(x) == 10:
+            print('Thank you! Wanna try again? if not, print "stop"')
+        elif x.isdigit() == False:
+            print('Please, use digits only!')
+        elif len(x)!= 10:
+            print('Your number should contain 10 digits only!')
 
 #Task3
 
 #The name check.
 
 name = 'artem'
-inp_name = input('Please, input your name: ')
-if inp_name.lower() == name:
-    print('True')
-else:
-    print('False')
+while True:
+    inp_name = input('Please, input your name: ')
+    if inp_name.lower() == name:
+        print('Yes! this name is correct! You won!')
+        break
+    else:
+        print('Sorry, but this name is not valid, try again please!')
