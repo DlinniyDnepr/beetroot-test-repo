62041e702db4e03c0dceb7ffd452782f84bb8ebc
DlinniyDnepr <63417182+DlinniyDnepr@users.noreply.github.com>
2020-04-26 21:27:12 +0300

    Update lesson4.py

--- lesson4.py
+++ lesson4.py
@@ -1,4 +1,4 @@
-#Task1
+print('Task1')
 # The greatest number
 import random
 
@@ -6,18 +6,49 @@ random_list = []
 while len(random_list) <= 10:
     random_list.append(random.randint(0, 100))
     if len(random_list) == 10:
-     print(random_list)
-     print(max(random_list))
+     print('Список из 10 рандомных чисел от 0 до 100', random_list)
+     print('Самое большое число в списке, это ', max(random_list))
 
 
-#Task2
+print('Task2')
+"""
+1 создать рандом список1
+2создать рандом список 2
+3 сравнить их через Set and set"""
+# random_list1 = []
+# random_list2 = []
+# index = 0
+# while True:
+#     random_list1.append(random.randint(0, 10))
+#     random_list2.append(random.randint(0, 10))
+#     index += 1
+#     item = random_list1[index]
+#     if item in random_list2:
+#         print(item)
+
 random_list1 = []
 random_list2 = []
-index = 0
 while len(random_list1) <= 10:
     random_list1.append(random.randint(0, 10))
+    if len(random_list1) == 10:
+     print('Первый рандомный список', random_list1)
+     break
+
+while len(random_list2) <= 10:
     random_list2.append(random.randint(0, 10))
-    index += 1
-    item = random_list1[index]
-    if item in random_list2:
-        print(item)
+    if len(random_list2) == 10:
+     print('Второй рандомный список', random_list2)
+
+itog = list(set(random_list1) & set(random_list2))
+print('Итоговый список включающий совпадающие числа ', itog)
+
+print('Task3')
+
+"""
+1) Составить список включающий все целые числа от 0 до 100
+2) найти все целые числа которые делятся на 7 но не делятся на 5
+3) создать список в который эти числа сохранить
+4) Распечатать этот список
+"""
+main_list = list(range(0, 101))
+print(main_list)
