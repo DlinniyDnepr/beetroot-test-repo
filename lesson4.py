de6d71943d597bd739d83f17ee0bd9d65b960012
DlinniyDnepr <63417182+DlinniyDnepr@users.noreply.github.com>
2020-05-05 15:36:23 +0300

    Update lesson4.py

--- lesson4.py
+++ lesson4.py
@@ -4,13 +4,13 @@ import random
 
 random_list = []
 while len(random_list) <= 10:
-    random_list.append(random.randint(0, 100))
+    random_list.append(random.randint(0, 101))
     if len(random_list) == 10:
      print('Список из 10 рандомных чисел от 0 до 100', random_list)
      print('Самое большое число в списке, это ', max(random_list))
 
 
-print('Task2')
+print('\nTask2')
 """
 1 создать рандом список1
 2создать рандом список 2
@@ -42,7 +42,7 @@ while len(random_list2) <= 10:
 itog = list(set(random_list1) & set(random_list2))
 print('Итоговый список включающий совпадающие числа ', itog)
 
-print('Task3')
+print('\nTask3')
 
 """
 1) Составить список включающий все целые числа от 0 до 100
@@ -50,11 +50,13 @@ print('Task3')
 3) создать список в который эти числа сохранить
 4) Распечатать этот список
 """
+output_list = []
 main_list = list(range(0, 101))
-print(main_list)
+print(f'Это список из всех целых чисел от 0 до 100 \n{main_list}')
 for index in main_list:
  if index%7 == 0 and index%5 != 0:
-  print([index])
-  # output_list = []
+  output_list.append(index)
+print(f'\nА это список из целыйх чисел которые делятся на 7 но не делятся на 5  {output_list}')
+
   # output_list = output_list.append(index)
   # print(output_list)
