1313853cf6f2448a1f3f36eb8aa1523ef5f9263b
DlinniyDnepr <63417182+DlinniyDnepr@users.noreply.github.com>
2020-05-05 20:38:44 +0300

    Create Lesson5.py

---
+++ Lesson5.py
@@ -0,0 +1,11 @@
+print('Task 1')
+"""
+Make a program that has some sentence (a string) on input 
+and returns a dict containing all unique words as keys 
+and the number of occurrences as values. 
+"""
+dict_string = {}
+string = input('Введи сюда какое либо предложение: ').split()
+# print(string)
+# string = string.split()
+print(string)
