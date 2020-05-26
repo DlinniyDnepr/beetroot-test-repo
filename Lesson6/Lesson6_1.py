21c2f97f3910f29bd5eb143e15072da6a249365d
DlinniyDnepr <63417182+DlinniyDnepr@users.noreply.github.com>
2020-05-26 20:14:45 +0300

    Create Lesson6_1.py

---
+++ Lesson6/Lesson6_1.py
@@ -0,0 +1,14 @@
+
+print('Task 1\n')
+
+"""A simple function.
+
+Create a simple function called favorite_movie, 
+which takes a string containing the name of your favorite movie. 
+The function should then print “My favorite movie is named {name}”.
+"""
+name = input('Type here your favourite movie: ')
+def favorite_movie ():
+        print('My favorite movie is named ', name)
+    
+favorite_movie()
